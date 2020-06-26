5c05591e0e38d8fe1589bb2baab8ffb1fef6b164
Rob <rlmccain@ncsu.edu>
2020-06-26 14:08:49 -0400

    This is my updated coded html and js files.

---
+++ script.js
@@ -0,0 +1,36 @@
+const date = new Date();
+
+const monthDays = document.querySelector(".days");
+
+const lastDay = new Date(date.getFullYear(),
+date.getMonth() + 1, 0)
+
+console.log(lastDay);
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+document.querySelector(".date h1").innerHTML
+= months[date.getMonth()];
+
+document.querySelector(".date p").innerHTML
+= date.toDateString();
+
+let days = "";
+
+for (let i = 1; i<= 31;i++) {
+    days += `<div>${i}</div>`;
+    monthDays.innerHTML = days;
+}
